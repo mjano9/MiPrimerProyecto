be5f0366fec5ec743e7a8ea913cad4694f4b0f43
mjano9 <m_jano@hotmail.com>
2020-11-10 01:24:22 -0500

    tercera linea

--- test.cpp
+++ test.cpp
@@ -5,6 +5,7 @@ int main()
     int string;
     printf("%s","Hola Mundo");
     printf("%s","\nEste es mi programa en C");
+    printf("%s","\nEste es mi tercera linea");
     string=0;
     return 0;
 }
